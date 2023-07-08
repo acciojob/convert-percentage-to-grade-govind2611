847d3cd165f7ed4b69e3e315d50364d6e9572643
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-07-09 04:00:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function calculateGrade(percentage) {
-  //your code here
+  if (typeof percentage === 'number' && !isNaN(percentage)) {
+    var roundedPercentage = Math.round(percentage);
+    if (roundedPercentage >= 90) {
+      return 'A';
+    } else if (roundedPercentage >= 80) {
+      return 'B';
+    } else if (roundedPercentage >= 70) {
+      return 'C';
+    } else if (roundedPercentage >= 60) {
+      return 'D';
+    } else {
+      return 'F';
+    }
+  } else {
+    return 'F';
+  }
 }
-const percentage = parseInt(prompt("Enter Percentage."));
+
+const percentage = parseInt(prompt('Enter Percentage.'));
 alert(calculateGrade(percentage));
